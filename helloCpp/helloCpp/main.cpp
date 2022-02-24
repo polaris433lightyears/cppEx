5ee5bea9ca54026c14281bc55c418a7cacb95531
Polaris433 <polaris433lightyears@gmail.com>
2022-02-24 09:54:01 +0800

    . helloCpp

---
+++ helloCpp/helloCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  helloCpp
+//
+//  Created by Jianpeng Pan on 24/2/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
